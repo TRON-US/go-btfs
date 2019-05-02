3352a5b00878a7a0890640f27174bcb165453ec2
Shengwen Yu <yushengwen@tron.network>
2019-05-02 14:07:31 -0700

    fix: update the install_btfs.sh script

--- install_btfs.sh
+++ install_btfs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd $GOPATH/src/github.com/TRON-US/go-btfs
 make install
 btfs init
 # swarm key
-cp $GOPATH/src/github.com/TRON-US/go-btfs/swarm.key ~/.btfs/swarm.key
+cp swarm.key ~/.btfs/swarm.key
 # bootstrap node
 btfs bootstrap rm --all
 btfs bootstrap add /ip4/3.18.120.107/tcp/4001/ipfs/QmcmRdAHQYTtpbs9Ud5rNx6WzHmU9WcYCrBneCSyKhMr7H
@@ -13,5 +12,3 @@ btfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", 
 btfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials "[\"true\"]"
 btfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 btfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-# start the daemon
-nohup btfs daemon </dev/null > /dev/null 2>&1 &
