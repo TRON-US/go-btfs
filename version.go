db9a8c0ca6a42248d15adb0257600401bbfa0c94
liujiaxun <liujiaxun@tron.network>
2019-06-06 10:41:18 -0700

    perf: update version.go version to 0.0.2

--- version.go
+++ version.go
@@ -4,6 +4,6 @@ package ipfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.0.1"
+const CurrentVersionNumber = "0.0.2"
 
 const ApiVersion = "/go-ipfs/" + CurrentVersionNumber + "/"
