e7978fb2afe533ee88b530fb21e6ac5b5314ce9b
Zhimin He <zhiminhe@gmail.com>
2019-06-29 07:38:10 -0700

    Update install_btfs.sh

--- install_btfs.sh
+++ install_btfs.sh
@@ -6,11 +6,7 @@ export GO111MODULE=on
 go clean -modcache
 make install
 btfs init
-# bootstrap node
-btfs bootstrap rm --all
-btfs bootstrap add /ip4/3.18.120.107/tcp/4001/ipfs/QmcmRdAHQYTtpbs9Ud5rNx6WzHmU9WcYCrBneCSyKhMr7H
-btfs bootstrap add /ip4/3.14.203.8/tcp/4001/ipfs/QmbsqP3GLrRRhGWwnXnb6gb7EFC9LAege333NBpn9cDXAv
-btfs bootstrap add /ip4/3.14.238.171/tcp/4001/ipfs/QmRb1Vi7JeNMVE2QVvCuWFU2J2qt6rn4pLf31CHyjt9GbB
+
 # cross-origin resource sharing
 btfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
 btfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
