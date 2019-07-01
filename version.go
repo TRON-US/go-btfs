036c061220ce77607fbbcc7b65db1879a57e34ea
Jin Liu <jinliu@Jins-MacBook-Pro.local>
2019-07-01 11:54:56 -0700

    update version number to 0.0.4

--- version.go
+++ version.go
@@ -4,6 +4,6 @@ package ipfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.0.3"
+const CurrentVersionNumber = "0.0.4"
 
 const ApiVersion = "/go-ipfs/" + CurrentVersionNumber + "/"
