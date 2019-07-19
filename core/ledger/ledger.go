3bb3f2c143c87503863dd558f3585a4aeb885509
Shelly Wei <guaichen07@gmail.com>
2019-07-19 10:47:31 -0700

    modify connection method instead from file

--- core/ledger/ledger.go
+++ core/ledger/ledger.go
@@ -5,7 +5,11 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/sha256"
-	"os"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"strings"
 	"time"
 
 	ledgerPb "github.com/TRON-US/go-btfs/core/ledger/pb"
@@ -21,16 +25,42 @@ import (
 var log = logging.Logger("ledger")
 var (
 	ledgerAddr     = "ledger.bt.co:443"
-	credentialPath = "/core/ledger/credential/public_key.pem"
+	certFile = `-----BEGIN CERTIFICATE-----
+MIIEADCCAuigAwIBAgIBADANBgkqhkiG9w0BAQUFADBjMQswCQYDVQQGEwJVUzEh
+MB8GA1UEChMYVGhlIEdvIERhZGR5IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBE
+YWRkeSBDbGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTA0MDYyOTE3
+MDYyMFoXDTM0MDYyOTE3MDYyMFowYzELMAkGA1UEBhMCVVMxITAfBgNVBAoTGFRo
+ZSBHbyBEYWRkeSBHcm91cCwgSW5jLjExMC8GA1UECxMoR28gRGFkZHkgQ2xhc3Mg
+MiBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASAwDQYJKoZIhvcNAQEBBQADggEN
+ADCCAQgCggEBAN6d1+pXGEmhW+vXX0iG6r7d/+TvZxz0ZWizV3GgXne77ZtJ6XCA
+PVYYYwhv2vLM0D9/AlQiVBDYsoHUwHU9S3/Hd8M+eKsaA7Ugay9qK7HFiH7Eux6w
+wdhFJ2+qN1j3hybX2C32qRe3H3I2TqYXP2WYktsqbl2i/ojgC95/5Y0V4evLOtXi
+EqITLdiOr18SPaAIBQi2XKVlOARFmR6jYGB0xUGlcmIbYsUfb18aQr4CUWWoriMY
+avx4A6lNf4DD+qta/KFApMoZFv6yyO9ecw3ud72a9nmYvLEHZ6IVDd2gWMZEewo+
+YihfukEHU1jPEX44dMX4/7VpkI+EdOqXG68CAQOjgcAwgb0wHQYDVR0OBBYEFNLE
+sNKR1EwRcbNhyz2h/t2oatTjMIGNBgNVHSMEgYUwgYKAFNLEsNKR1EwRcbNhyz2h
+/t2oatTjoWekZTBjMQswCQYDVQQGEwJVUzEhMB8GA1UEChMYVGhlIEdvIERhZGR5
+IEdyb3VwLCBJbmMuMTEwLwYDVQQLEyhHbyBEYWRkeSBDbGFzcyAyIENlcnRpZmlj
+YXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQAD
+ggEBADJL87LKPpH8EsahB4yOd6AzBhRckB4Y9wimPQoZ+YeAEW5p5JYXMP80kWNy
+OO7MHAGjHZQopDH2esRU1/blMVgDoszOYtuURXO1v0XJJLXVggKtI3lpjbi2Tc7P
+TMozI+gciKqdi0FuFskg5YmezTvacPd+mSYgFFQlq25zheabIZ0KbIIOqPjCDPoQ
+HmyW74cNxA9hi63ugyuV+I6ShHI56yDqg+2DzZduCLzrTia2cyvk0/ZM/iZx4mER
+dEr/VxqHD3VILs9RaRegAhJhldXRQLIQTO7ErBBDpqWeCtWVYpoNz4iCxTIM5Cuf
+ReYNnyicsbkqWletNw+vHX/bvZ8=
+-----END CERTIFICATE-----`
 )
 
 func LedgerConnection() (*grpc.ClientConn, error) {
-	wd, err := os.Getwd()
-	credential, err := credentials.NewClientTLSFromFile(wd+credentialPath, "")
+	b, err := ioutil.ReadAll(strings.NewReader(certFile))
 	if err != nil {
-		log.Error("fail to load credential: ", err)
 		return nil, err
 	}
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(b) {
+		return nil, fmt.Errorf("credentials: failed to append certificates")
+	}
+	credential := credentials.NewTLS(&tls.Config{RootCAs: cp})
 	conn, err := grpc.Dial(ledgerAddr, grpc.WithTransportCredentials(credential))
 	if err != nil {
 		return nil, err
@@ -41,7 +71,7 @@ func LedgerConnection() (*grpc.ClientConn, error) {
 func CloseConnection(conn *grpc.ClientConn) {
 	if conn != nil {
 		if err := conn.Close(); err != nil {
-			log.Error("Failed to close connection: %v", err)
+			log.Error("Failed to close connection: ", err)
 		}
 	}
 }
@@ -108,7 +138,7 @@ func CreateChannel(ctx context.Context, ledgerClient ledgerPb.ChannelsClient, ch
 func CloseChannel(ctx context.Context, ledgerClient ledgerPb.ChannelsClient, signedChannelState *ledgerPb.SignedChannelState) error {
 	closed, err := ledgerClient.CloseChannel(ctx, signedChannelState)
 	if err != nil {
-		log.Error("channel fail to close:", closed.GetState().Channel)
+		log.Error("channel fail to close: ", closed.GetState().Channel)
 		return err
 	}
 	return nil
