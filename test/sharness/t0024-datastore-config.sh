a256226a11995b3809e9914cd8f11812c7637b44
richard lee <richardlee@tron.network>
2019-07-25 14:58:23 -0700

    BTFSINFRA-339 fix more sharness tests

--- test/sharness/t0024-datastore-config.sh
+++ test/sharness/t0024-datastore-config.sh
@@ -4,28 +4,28 @@ test_description="Test datastore config"
 
 . lib/test-lib.sh
 
-test_init_ipfs
+test_init_btfs
 
-test_launch_ipfs_daemon
-test_kill_ipfs_daemon
+test_launch_btfs_daemon
+test_kill_btfs_daemon
 
 SPEC_NOSYNC=$(cat ../t0024-files/spec-nosync)
 
 SPEC_NEWSHARDFUN=$(cat ../t0024-files/spec-newshardfun)
 
 test_expect_success "change runtime value in spec config" '
-  ipfs config --json Datastore.Spec "$SPEC_NOSYNC"
+  btfs config --json Datastore.Spec "$SPEC_NOSYNC"
 '
 
-test_launch_ipfs_daemon
-test_kill_ipfs_daemon
+test_launch_btfs_daemon
+test_kill_btfs_daemon
 
 test_expect_success "change on-disk value in spec config" '
-  ipfs config --json Datastore.Spec "$SPEC_NEWSHARDFUN"
+  btfs config --json Datastore.Spec "$SPEC_NEWSHARDFUN"
 '
 
 test_expect_success "can not launch daemon after on-disk value change" '
-  test_must_fail ipfs daemon
+  test_must_fail btfs daemon
 '
 
 test_done
