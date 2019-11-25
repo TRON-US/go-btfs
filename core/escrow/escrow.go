ef8fccb5f123d73df07c36733b9cf5d20925d300
Shelly Wei <guaichen07@gmail.com>
2019-11-25 12:08:55 -0800

    escrow package

--- core/escrow/escrow.go
+++ core/escrow/escrow.go
@@ -1,1 +1,21 @@
 package escrow
+
+import (
+	"github.com/gogo/protobuf/proto"
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	//"github.com/libp2p/go-libp2p-core/peer"
+	escrowpb "github.com/tron-us/go-btfs-common/protos/escrow"
+)
+
+func NewContract(id string, payerID string, recverID string, price int64) *escrowpb.EscrowContract {
+	payerAddr := []byte(payerID)
+	recverAddr := []byte(recverID)
+	return &escrowpb.EscrowContract{
+		ContractId: []byte(id),
+		BuyerAddress: payerAddr,
+		SellerAddress: recverAddr,
+		Amount: price,
+	}
+}
+
+
