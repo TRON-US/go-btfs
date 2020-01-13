07cd87c34fbcd98db9a03d8d3ca2366826a86f6d
Steve Yeom <steve.yeom@tron.network>
2020-01-13 15:28:50 -0800

    BTFS-1356: ran go fmt

--- pin/pin.go
+++ pin/pin.go
@@ -707,7 +707,7 @@ func (p *pinner) HasExpiration(c cid.Cid) (bool, error) {
 	if p.recursePin.Has(c) && p.recursePinMap.HasExpiration(c) {
 		return true, nil
 	}
-	
+
 	visitedSet := cid.NewSet()
 	for _, k := range p.recursePin.Keys() {
 		if !p.recursePinMap.HasExpiration(k) {
