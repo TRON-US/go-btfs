489feef52ebace22971471ba8a80504e4654ef70
Eric Chen <eric@tron.network>
2020-02-13 13:00:23 -0800

    Fix shard timeout not ending when session ended.

--- core/commands/storage.go
+++ core/commands/storage.go
@@ -470,7 +470,7 @@ func retryMonitor(ctx context.Context, api coreiface.CoreAPI, ss *storage.FileCo
 
 			// monitor each steps if error or time out happens, retry
 			// TODO: Change steps
-			for curState := 0; curState < len(storage.StdStateFlow); {
+			for curState := 0; curState < storage.CompleteState && !ss.SessionEnded(); {
 				select {
 				case shardRes := <-shard.RetryChan:
 					if !shardRes.Succeed {
