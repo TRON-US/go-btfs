afddc1b86136b318ee78568ea29df94acb01e6ff
Robin Lai <laipogo@gmail.com>
2020-03-26 11:50:36 +0800

    BTFS-1670: Optimize local hosts swarm connect in the background

--- core/commands/store/hosts/hosts.go
+++ core/commands/store/hosts/hosts.go
@@ -3,14 +3,18 @@ package hosts
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/TRON-US/go-btfs/core"
 	"github.com/TRON-US/go-btfs/core/commands/cmdenv"
 	"github.com/TRON-US/go-btfs/core/commands/storage"
+	"github.com/TRON-US/go-btfs/core/coreapi"
 	"github.com/TRON-US/go-btfs/core/hub"
 
 	cmds "github.com/TRON-US/go-btfs-cmds"
 	hubpb "github.com/tron-us/go-btfs-common/protos/hub"
+
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 const (
@@ -104,15 +108,39 @@ Mode options include:` + hub.AllModeHelpText,
 		if err != nil {
 			return err
 		}
-
 		return SyncHosts(req.Context, n, mode)
 	},
 }
 
 func SyncHosts(ctx context.Context, node *core.IpfsNode, mode string) error {
+	api, err := coreapi.NewCoreAPI(node)
+	if err != nil {
+		return err
+	}
 	nodes, err := hub.QueryHosts(ctx, node, mode)
 	if err != nil {
 		return err
 	}
-	return storage.SaveHostsIntoDatastore(ctx, node, mode, nodes)
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	hs := make([]*hubpb.Host, 0)
+	for _, h := range nodes {
+		wg.Add(1)
+		go func(h *hubpb.Host) {
+			if err := api.Swarm().Connect(ctx, peer.AddrInfo{ID: peer.ID(h.NodeId)}); err != nil {
+				mu.Lock()
+				// push back
+				hs = append(hs, h)
+				mu.Unlock()
+			} else {
+				mu.Lock()
+				// push front
+				hs = append([]*hubpb.Host{h}, hs...)
+				mu.Unlock()
+			}
+			wg.Done()
+		}(h)
+	}
+	wg.Wait()
+	return storage.SaveHostsIntoDatastore(ctx, node, mode, hs)
 }
