ac2a0968ea19e6009e378c180957a1fafbe02ab9
“NathanQiu666” <393546842@qq.com>
2020-04-18 01:47:21 +0800

    BTFS-1214

---
+++ core/commands/restart.go
@@ -0,0 +1,56 @@
+package commands
+
+import (
+	"github.com/TRON-US/go-btfs-cmds"
+	"github.com/TRON-US/go-btfs/core/commands/store/path"
+	"github.com/cenkalti/backoff/v3"
+	"os/exec"
+	"time"
+)
+
+var daemonStartup = func() *backoff.ExponentialBackOff {
+	bo := backoff.NewExponentialBackOff()
+	bo.InitialInterval = 2 * time.Second
+	bo.MaxElapsedTime = 300 * time.Second
+	bo.Multiplier = 1
+	bo.MaxInterval = 2 * time.Second
+	return bo
+}()
+
+var restartCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline: "Restart the daemon.",
+		ShortDescription: `
+Shutdown the runnning daemon and start a new daemon process.
+And if specified a new btfs path, it will be applied.
+`,
+	},
+
+	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+		shutdownCmd := exec.Command("btfs", "shutdown")
+		if err := shutdownCmd.Run(); err != nil {
+			return err
+		}
+
+		if err := path.MoveFolder(); err != nil {
+			return err
+		}
+
+		if err := path.WriteProperties(); err != nil {
+			return err
+		}
+
+		err := backoff.Retry(func() error {
+			daemonCmd := exec.Command("btfs", "daemon")
+			if err := daemonCmd.Run(); err != nil {
+				return err
+			}
+			return nil
+		}, daemonStartup)
+
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+}
---
+++ core/commands/store/path/path.go
@@ -0,0 +1,210 @@
+package path
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/TRON-US/go-btfs-cmds"
+	logging "github.com/ipfs/go-log"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+const (
+	storeDir = ".btfs"
+	fileName = "path.properties"
+)
+
+var btfsPath string
+var filePath string
+var storePath string
+var originPath string
+var log = logging.Logger("core/commands/store/path")
+
+var PathCmd = &cmds.Command{
+	Helptext: cmds.HelpText{
+		Tagline: "Modify the Host storage folder path for BTFS client.",
+		ShortDescription: `
+The default local repository path is located at ~/.btfs folder, in order to
+improve the hard disk space usage, provide the function to change the original 
+storage location, a specified path as a parameter need to be passed.
+`,
+	},
+	Arguments: []cmds.Argument{
+		cmds.StringArg("path_name", true, true, "New BTFS Path.").EnableStdin(),
+	},
+
+	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+		storePath = strings.Trim(req.Arguments[0], " ")
+
+		if storePath == "" {
+			return fmt.Errorf("path is not defined")
+		}
+
+		if btfsPath != "" {
+			if btfsPath != storePath {
+				originPath = btfsPath
+			} else {
+				return fmt.Errorf("specifed path is same with current path")
+			}
+		} else if home, err := Home(); err == nil && home != "" {
+			originPath = home
+		} else {
+			return fmt.Errorf("can not find the original stored path")
+		}
+
+		if CheckExist(storePath) == false {
+			err := os.MkdirAll(storePath, os.ModePerm)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if CheckDirEmpty(filepath.Join(storePath, storeDir)) == false {
+			return fmt.Errorf("path is occupied")
+		}
+
+		restartCmd := exec.Command("btfs", "restart")
+		if err := restartCmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	},
+}
+
+func init() {
+	getPropertiesPath()
+	SetEnvVariables()
+}
+
+func WriteProperties() error {
+	if CheckExist(filePath) == false {
+		newFile, err := os.Create(filePath)
+		defer newFile.Close()
+		if err != nil {
+			return err
+		}
+	}
+	data := []byte(storePath)
+	err := ioutil.WriteFile(filePath, data, 0666)
+	if err == nil {
+		fmt.Printf("Storage location was reset in %v\n", storePath)
+	}
+	return err
+}
+
+func MoveFolder() error {
+	// make dir does not contain .btfs, but move need to specify .btfs
+	err := os.Rename(filepath.Join(originPath, storeDir), filepath.Join(storePath, storeDir))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadProperties(filePath string) string {
+	f, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Errorf("Read properties fail: [%v]\n", err)
+	}
+	return string(f)
+}
+
+func CheckDirEmpty(dirname string) bool {
+	dir, _ := ioutil.ReadDir(dirname)
+	if len(dir) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func Home() (string, error) {
+	user, err := user.Current()
+	if nil == err {
+		return user.HomeDir, nil
+	}
+	// cross compile support
+	if "windows" == runtime.GOOS {
+		return homeWindows()
+	}
+	// Unix-like system, so just assume Unix
+	return homeUnix()
+}
+
+func homeUnix() (string, error) {
+	// First prefer the HOME environmental variable
+	if home := os.Getenv("HOME"); home != "" {
+		return home, nil
+	}
+	// If that fails, try the shell
+	var stdout bytes.Buffer
+	cmd := exec.Command("sh", "-c", "eval echo ~$USER")
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	result := strings.TrimSpace(stdout.String())
+	if result == "" {
+		return "", errors.New("blank output when reading home directory")
+	}
+	return result, nil
+}
+
+func homeWindows() (string, error) {
+	drive := os.Getenv("HOMEDRIVE")
+	path := os.Getenv("HOMEPATH")
+	home := drive + path
+	if drive == "" || path == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	if home == "" {
+		return "", errors.New("HOMEDRIVE, HOMEPATH, and USERPROFILE are blank")
+	}
+	return home, nil
+}
+
+func getPropertiesPath() {
+	var propertiesPath string
+	if home, err := Home(); err == nil {
+		if "windows" == runtime.GOOS {
+			propertiesPath = filepath.Join(home, "btfs")
+		} else {
+			propertiesPath = filepath.Join(home, "btfs/bin")
+		}
+	}
+
+	if CheckExist(propertiesPath) == false {
+		err := os.MkdirAll(propertiesPath, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+	filePath = filepath.Join(propertiesPath, fileName)
+}
+
+func SetEnvVariables() {
+	if CheckExist(filePath) {
+		btfsPath = ReadProperties(filePath)
+		if btfsPath != "" {
+			newPath := filepath.Join(btfsPath, storeDir)
+			err := os.Setenv("BTFS_PATH", newPath)
+			if err != nil {
+				log.Errorf("cannot set env variable of BTFS_PATH: [%v] \n", err)
+			}
+		}
+	}
+}
+
+func CheckExist(pathName string) bool {
+	_, err := os.Stat(pathName)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
