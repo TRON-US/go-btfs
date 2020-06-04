58193aa498a99eed81c55609805bc262afabab90
Eric Chen <eric@tron.network>
2020-06-03 21:15:41 -0700

    update version 1.2.0

--- version.go
+++ version.go
@@ -4,6 +4,6 @@ package btfs
 var CurrentCommit string
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "1.1.3"
+const CurrentVersionNumber = "1.2.0"
 
 const ApiVersion = "/go-btfs/" + CurrentVersionNumber + "/"
