3d9c6f335f4bd86796ec1a1c607cfc8d685e8fd6
Eric Chen <eric@tron.network>
2020-06-29 16:52:39 -0700

    Fix import

--- core/commands/id.go
+++ core/commands/id.go
@@ -19,7 +19,7 @@ import (
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pstore "github.com/libp2p/go-libp2p-core/peerstore"
 	kb "github.com/libp2p/go-libp2p-kbucket"
-	identify "github.com/libp2p/go-libp2p/p2p/protocol/identify"
+	//identify "github.com/libp2p/go-libp2p/p2p/protocol/identify"
 )
 
 const offlineIdErrorMessage = `'btfs id' currently cannot query information on remote
