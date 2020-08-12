ee9af30799fa0a8dbf698c43d87e623bf6a06b32
Robin <robin.lai@tron.network>
2020-08-12 10:00:21 +0800

    fix port dir in windows

--- core/wallet/speed_windows.go
+++ core/wallet/speed_windows.go
@@ -2,9 +2,16 @@
 
 package wallet
 
-const (
-	portPath = "%AppData%/../Local/BitTorrentHelper/"
-)
+import "fmt"
+import "os"
+
+var portPath = func() string {
+	home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+	if home == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	return home + "\\AppData\\Local\\BitTorrentHelper\\"
+}()
 
 func validateOs() error {
 	return nil
