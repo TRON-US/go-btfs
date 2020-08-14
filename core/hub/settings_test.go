880bf37f21f89736f41a0b7701af123ff0a24cdb
Eric Chen <eric@tron.network>
2020-08-14 02:47:12 -0700

    Fix test

--- core/hub/settings_test.go
+++ core/hub/settings_test.go
@@ -14,7 +14,7 @@ func TestGetSettings(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defNs := &nodepb.Node_Settings{StoragePriceAsk: 250000, StorageTimeMin: 30}
+	defNs := &nodepb.Node_Settings{StoragePriceAsk: 250000, StorageTimeMin: 30, StoragePriceDefault: 250000}
 	if !reflect.DeepEqual(ns, defNs) {
 		t.Fatal("default settings not equal")
 	}
