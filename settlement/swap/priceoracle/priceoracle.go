3827b2397cd43cfc13b1a44d339336bb0f02a46e
laocheng.cheng <laocheng.cheng@junlin.local>
2021-09-16 17:25:27 +0800

    del settlement/swap/priceoracle's deduction

--- settlement/swap/priceoracle/priceoracle.go
+++ settlement/swap/priceoracle/priceoracle.go
@@ -27,7 +27,6 @@ type service struct {
 	priceOracleAddress common.Address
 	transactionService transaction.Service
 	exchangeRate       *big.Int
-	deduction          *big.Int
 	timeDivisor        int64
 	quitC              chan struct{}
 }
@@ -36,9 +35,9 @@ type Service interface {
 	io.Closer
 	// CurrentRates returns the current value of exchange rate and deduction
 	// according to the latest information from oracle
-	CurrentRates() (exchangeRate *big.Int, deduction *big.Int, err error)
+	CurrentRates() (exchangeRate *big.Int, err error)
 	// GetPrice retrieves latest available information from oracle
-	GetPrice(ctx context.Context) (*big.Int, *big.Int, error)
+	GetPrice(ctx context.Context) (*big.Int, error)
 	Start()
 }
 
@@ -52,7 +51,6 @@ func New(logger logging.Logger, priceOracleAddress common.Address, transactionSe
 		priceOracleAddress: priceOracleAddress,
 		transactionService: transactionService,
 		exchangeRate:       big.NewInt(0),
-		deduction:          nil,
 		quitC:              make(chan struct{}),
 		timeDivisor:        timeDivisor,
 	}
@@ -68,13 +66,12 @@ func (s *service) Start() {
 	go func() {
 		defer cancel()
 		for {
-			exchangeRate, deduction, err := s.GetPrice(ctx)
+			exchangeRate, err := s.GetPrice(ctx)
 			if err != nil {
 				s.logger.Errorf("could not get price: %v", err)
 			} else {
-				s.logger.Tracef("updated exchange rate to %d and deduction to %d", exchangeRate, deduction)
+				s.logger.Tracef("updated exchange rate to %d", exchangeRate)
 				s.exchangeRate = exchangeRate
-				s.deduction = deduction
 			}
 
 			ts := time.Now().Unix()
@@ -96,49 +93,41 @@ func (s *service) Start() {
 	}()
 }
 
-func (s *service) GetPrice(ctx context.Context) (*big.Int, *big.Int, error) {
+func (s *service) GetPrice(ctx context.Context) (*big.Int, error) {
 	callData, err := priceOracleABI.Pack("getPrice")
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	result, err := s.transactionService.Call(ctx, &transaction.TxRequest{
 		To:   &s.priceOracleAddress,
 		Data: callData,
 	})
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	results, err := priceOracleABI.Unpack("getPrice", result)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	if len(results) != 2 {
-		return nil, nil, errDecodeABI
+		return nil, errDecodeABI
 	}
 
 	exchangeRate, ok := abi.ConvertType(results[0], new(big.Int)).(*big.Int)
 	if !ok || exchangeRate == nil {
-		return nil, nil, errDecodeABI
+		return nil, errDecodeABI
 	}
 
-	deduction, ok := abi.ConvertType(results[1], new(big.Int)).(*big.Int)
-	if !ok || deduction == nil {
-		return nil, nil, errDecodeABI
-	}
-
-	return exchangeRate, deduction, nil
+	return exchangeRate, nil
 }
 
-func (s *service) CurrentRates() (exchangeRate, deduction *big.Int, err error) {
+func (s *service) CurrentRates() (exchangeRate *big.Int, err error) {
 	if s.exchangeRate.Cmp(big.NewInt(0)) == 0 {
-		return nil, nil, errors.New("exchange rate not yet available")
+		return nil, errors.New("exchange rate not yet available")
 	}
-	if s.deduction == nil {
-		return nil, nil, errors.New("deduction amount not yet available")
-	}
-	return s.exchangeRate, s.deduction, nil
+	return s.exchangeRate, nil
 }
 
 func (s *service) Close() error {
