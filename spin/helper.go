a00c8b17f3a995e8f68c35834ff940c09a4fc2c0
Matt <yrp1990@gmail.com>
2021-10-22 15:37:21 +0800

    Simplify log output to be more readable

--- spin/helper.go
+++ spin/helper.go
@@ -16,10 +16,10 @@ func periodicSync(period, timeout time.Duration, msg string, syncFunc func(conte
 		err := syncFunc(ctx)
 		if err != nil {
 			if strings.Contains(err.Error(), "Cannot query to get stats, please wait for host validation") {
-				log.Errorf("Currently syncing the BTFS network, " +
+				log.Infof("Currently syncing the BTFS network, " +
 					"which may last several hours depending on network conditions. Please be patient.")
 			} else if strings.Contains(err.Error(), "Cannot query to get hosts") {
-				log.Errorf("Cannot query to get hosts. " +
+				log.Infof("Cannot query to get hosts. " +
 					"If you are not a renter, i.e. don't need to upload files, you can ignore it.")
 			} else {
 				log.Errorf("Failed to sync %s: %s", msg, err)
