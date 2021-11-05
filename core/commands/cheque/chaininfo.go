e83225c356e108ea5c4fdffb9a520b0089559beb
ken.sun <ken.sun@tron.network>
2021-11-05 14:09:42 +0800

    Add receiver addr in chaininfo cmd

--- core/commands/cheque/chaininfo.go
+++ core/commands/cheque/chaininfo.go
@@ -9,8 +9,10 @@ import (
 )
 
 type ChainInfoRet struct {
-	ChainId  int64  `json:"chain_id"`
-	NodeAddr string `json:"node_addr"`
+	ChainId        int64  `json:"chain_id"`
+	NodeAddr       string `json:"node_addr"`
+	ChequeBookAddr string `json:"cheque_book_addr"`
+	ReceiverAddr   string `json:"receiver_addr"`
 }
 
 var ChequeChainInfoCmd = &cmds.Command{
@@ -18,16 +20,23 @@ var ChequeChainInfoCmd = &cmds.Command{
 		Tagline: "Show current chain info.",
 	},
 	Run: func(req *cmds.Request, res cmds.ResponseEmitter, env cmds.Environment) error {
+		receiver, err := chain.SettleObject.ChequebookService.Receiver()
+		if err != nil {
+			return err
+		}
 
 		return cmds.EmitOnce(res, &ChainInfoRet{
-			ChainId:  chain.ChainObject.ChainID,
-			NodeAddr: chain.ChainObject.OverlayAddress.String(),
+			ChainId:        chain.ChainObject.ChainID,
+			NodeAddr:       chain.ChainObject.OverlayAddress.String(),
+			ChequeBookAddr: chain.SettleObject.ChequebookService.Address().String(),
+			ReceiverAddr:   receiver.String(),
 		})
 	},
 	Type: &ChainInfoRet{},
 	Encoders: cmds.EncoderMap{
 		cmds.Text: cmds.MakeTypedEncoder(func(req *cmds.Request, w io.Writer, out *ChainInfoRet) error {
-			_, err := fmt.Fprintf(w, "chain id: %d\t node addr: %s\n", out.ChainId, out.NodeAddr)
+			_, err := fmt.Fprintf(w, "chain id: %d\t node addr: %s\t chequebook addr:%s\treceiver addr:%s\n", out.ChainId,
+				out.NodeAddr, out.ChequeBookAddr, out.ReceiverAddr)
 			return err
 		}),
 	},
