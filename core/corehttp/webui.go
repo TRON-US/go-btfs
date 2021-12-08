6c5a2bca8d1d9cc2b551d58e2a385f44efdf6db8
sunbx <sunbenxin007@gmail.com>
2021-12-08 11:21:32 +0800

    Revert "Use new webui front end build(dashboard)"

--- core/corehttp/webui.go
+++ core/corehttp/webui.go
@@ -1,6 +1,6 @@
 package corehttp
 
-const WebUIPath = "/btns/16Uiu2HAm8ZYAmsEXE6qBoSeRTeGGR2KVPUZBLBf1dEh9y28ANMMt"
+const WebUIPath = "/btns/QmWJWGxKKaqZUW4xga2BCzT5FBtYDL8Cc5Q5jywd6xPt1g"
 
 // this is a list of all past webUI paths.
 var WebUIPaths = []string{
